d428c7303a080372bafa78956ec6ecf08375309e
MOKSHITH V <85726740+Mokshithv@users.noreply.github.com>
2025-06-06 11:13:22 +0530

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -1,12 +1,9 @@
-// Prim's Algorithm in C to find Minimum Cost Spanning Tree
-
 #include <stdio.h>
 #include <stdbool.h>
 
 #define INF 9999999
-#define V 5 // Number of vertices
+#define V 5 
 
-// Graph represented as an adjacency matrix
 int G[V][V] = {
   {0, 9, 75, 0, 0},
   {9, 0, 95, 19, 42},
@@ -16,20 +13,17 @@ int G[V][V] = {
 };
 
 int main() {
-  int no_edge = 0;  // Number of edges in MST
-  int selected[V];  // Tracks selected vertices
+  int no_edge = 0;  
+  int selected[V];  
   int total_cost = 0;
   selected[0] = true;
 
   int x, y;
 
-  // Initially no vertex is selected
+  
   for (int i = 0; i < V; i++) {
     selected[i] = false;
   }
-
-  // Start from the first vertex
-  // Edge endpoints
 
   printf("Edge : Weight\n");
 
